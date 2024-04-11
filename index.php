8f9a9a556802d67d983982a78b30c9426c12327a
bezhan2009 <karimovbezan0@gmail.com>
2024-04-11 23:09:49 +0300

    Checking

--- index.php
+++ index.php
@@ -5,9 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ToDoTeam</title>
 </head>
+    <form action="">
+        <input type="text" name="name">
+        <button>Say Hello</button>
+    </form>
+
+
 <body>
     <?
-        echo "Just Checking."
+        name = $_POST['name']
+        if name {
+            echo "Hello, $name";
+        }
+        else {
+            echo "Please enter your name";
+        }
     ?>
 </body>
 </html>
