9189da41005d2ae4fa7d953bdb371cd6b15024b5
Chebupelli <kremenvov11@gmail.com>
2024-06-02 19:59:22 +0300

    add: create profile page

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/main.css" rel="stylesheet" type="text/css">
+    <title>ToDoTeam</title>
+</head>
+
+<body>
+    <div align="center" class="main">
+        <h1>
+            <?php
+                echo "Здравствуйте уважаемый пользователь!<br>";
+                echo "Вы вошли на сайт ToDoTeam!<br>";
+                echo "Ваше время и дата: ". date("H:i:s d.m.Y"). "<br>";
+                echo "На данный момент мы не можем предоставить вам функционал<br>нашего проекта из за того что мы пока не начали активно разрабатывать приложения ToDoTeam :("
+            ?>
+        </h1>
+    </div>
+</body>
+</html>
